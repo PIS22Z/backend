710af033cf2595b35637e9bfa50e69140e0f7ffc
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2022-11-30 21:40:04 +0100

    PIS-35: Jako zamawiający mogę opłacić sfinalizowane zamówienie

---
+++ application/src/main/kotlin/dev/hypest/pis/restaurants/adapter/in/event/OrderPaidEventListener.kt
@@ -0,0 +1,26 @@
+@file:Suppress("InvalidPackageDeclaration")
+
+package dev.hypest.pis.restaurants.adapter.`in`.event
+
+import dev.hypest.pis.common.logger
+import dev.hypest.pis.payments.OrderPaidEvent
+import dev.hypest.pis.restaurants.adapter.`in`.mapper.OrderPaidEventMapper.mapToCreateActiveOrderCommand
+import dev.hypest.pis.restaurants.domain.activeorder.CreateActiveOrderHandler
+import io.micronaut.rabbitmq.annotation.Queue
+import io.micronaut.rabbitmq.annotation.RabbitListener
+
+@RabbitListener
+class OrderPaidEventListener(
+    private val createActiveOrderHandler: CreateActiveOrderHandler
+) {
+
+    private val log by logger()
+
+    @Queue("order-paid")
+    fun receive(event: OrderPaidEvent) {
+        log.info("Received event: $event")
+
+        val command = mapToCreateActiveOrderCommand(event)
+        createActiveOrderHandler.create(command)
+    }
+}
